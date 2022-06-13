182765c501b454645c22d786bf1080f804951c82
Shubhra <sshswain@publicisgroupe.net>
2022-06-13 06:24:42 -0700

    - DayNight theme - Splash activity

--- app/src/main/java/com/example/razorpayifsc/MainActivity.kt
+++ app/src/main/java/com/example/razorpayifsc/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.razorpayifsc
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.razorpayifsc.databinding.ActivityMainBinding
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
