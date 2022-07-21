542b3be46a8b5893fc3dc70328c1e491b6762719
sshswain <shakti.subhra1995@gmail.com>
2022-07-21 17:12:50 +0530

    - Imported missing module

--- app/src/main/java/com/example/razorpayifsc/di/NetworkModule.kt
+++ app/src/main/java/com/example/razorpayifsc/di/NetworkModule.kt
@@ -18,6 +18,7 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -29,6 +30,7 @@ class NetworkModule {
         private const val READ_TIMEOUT = 30
         private const val WRITE_TIMEOUT = 30
         private const val CONNECTION_TIMEOUT = 10
+        private const val CACHE_SIZE_BYTES = 10 * 1024 * 1024L // 10 MB
     }
 
     @Singleton
@@ -69,6 +71,13 @@ class NetworkModule {
             // hear you can add all headers you want by calling 'requestBuilder.addHeader(name ,  value)'
             it.proceed(requestBuilder.build())
         }
+    }
+
+    @Provides
+    @Singleton
+    internal fun provideCache(context: Context): Cache {
+        val httpCacheDirectory = File(context.cacheDir.absolutePath, "HttpCache")
+        return Cache(httpCacheDirectory, CACHE_SIZE_BYTES)
     }
 
     @Provides
